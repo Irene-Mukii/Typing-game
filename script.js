4b822d7b94176d5a6fa151ef6561606a6b1a693c
renniequin@gmail.com <renniequin@gmail.com>
2022-09-14 20:53:53 +0300

    compares the first leter to the imput and proceeds with instructions, base conditions work but there is an error where it only has 2 elements after joining and storing the quote in a word array(even so the second element fails to highlight

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const quotes = [
     'I never make exceptions. An exception disproves the rule.',
     'What one man can invent another can discover.',
     'Nothing clears up a case so much as stating it to another person.',
-    'Education never ends, Watson. It is a series of lessons, with the greatest for the last.'
+    'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
+    'WORK'
 ];
 
 //empty array to store the words inputed
@@ -19,8 +20,6 @@ let startTime = Date.now();
 const quoteElement = document.getElementById('quote');
 const messageElement = document.getElementById('message');
 const typedValueElement = document.getElementById('typed-value');
-const elapsedTimeElement = document.getElementsByClassName('elapsed-time');
-
 //start logic
 
 document.getElementById('start-btn').addEventListener('click', () => {
@@ -30,24 +29,72 @@ document.getElementById('start-btn').addEventListener('click', () => {
     const quote = quotes[quoteIndex];
     
 
-    //put quote int0 an array of words
+    //put quote into an array of words
     words = quote.split(' '); //has to have a space so it splits when space is reached.
 
     wordIndex = 0;  // --resets word index to 0 not sure id it is necessary, bit maybe we will be reducing array not just checking indexes.
 
     //UI updates
-    const spanWords = words.map((word)=> {return `<span> ${word}</span`}); //an array to store each word from the quote  --we have added span elements to the wuote paragraph in html
-    quoteElement.innerHTML = spanWords.join(' ');   //display the quote --needs a space as well to know what seperates diff indices
-    quoteElement.childNodes[0].className = 'highlight'; //we give a class to the indexed span element
+    const spanWords = words.map(function (word) {return `<span> ${word}</span`});
+    console.log(spanWords.length)
+     //an array to store each word from the quote  --we have added span elements to the wuote paragraph in html --------------------------------------------------------------------------
+     
+    quoteElement.innerHTML = spanWords.join(' ');  
+    console.log(quoteElement.childNodes.length)
+    //display the quote --needs a space as well to know what seperates diff indices
+    quoteElement.childNodes[0].className = 'highlight';
+    console.log(quoteElement.length)
+    //we give a class to the indexed span element
     messageElement.innerHTML = ''; //clears any message
 
     //setup the text box
     //clears textbox
     typedValueElement.value = '';
-    //sets focus???
+    //sets focus  --------------------------------------------------------------------------------------------------------------------------???
     typedValueElement.focus();
     
-
     //starts the timer
     startTime = new Date().getTime();
-})
+});
+
+
+
+typedValueElement.addEventListener('input', ()=>{
+    //get the current word
+    const currentWord = words[wordIndex];
+    //get the current value
+    const typedValue = typedValueElement.value;
+
+    if(typedValue === currentWord && wordIndex === words.length-1){
+        //base case of having one word in a quote or quote ending.
+        const elapsedTime = new Date().getTime() - startTime;
+        const message = `CONGRATULATIONS! You have finished in ${elapsedTime / 1000} seconds`;
+        messageElement.innerHTML = message;
+    }else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) { //end of word and checking for correctness
+        //clear input area
+        typedValueElement.value = '';
+        //move to next word
+        wordIndex++;
+        
+
+        for (const wordElement of quoteElement.childNodes){
+            console.log(wordElement);
+            wordElement.className = '';
+            
+            console.log('Iam working' + quoteElement.childNodes.length + wordIndex)
+        }
+        //-------------------------------------------------------------------------------------------------
+        // highlight the new word
+        quoteElement.childNodes[wordIndex].className = 'highlight';
+        console.log(quoteElement.childNodes[wordIndex].className);
+    }
+    else if (currentWord.startsWith(typedValue)){
+        //currently correct   -----------------------------------------------------------------------------????
+        //highlight the next word
+        typedValueElement.className = '';
+    }else {
+        //error state
+        typedValueElement.className = 'error';
+    }
+
+});
