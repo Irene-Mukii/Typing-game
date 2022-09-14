bdc1acf0bfc8a68da2d5867208800140c4fdeb4a
renniequin@gmail.com <renniequin@gmail.com>
2022-09-14 18:16:35 +0300

    declares the variables to store the data we want to manipulate upon certain events

---
+++ script.js
@@ -0,0 +1,21 @@
+//all our quotes for now
+
+const quotes = [
+    'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
+    'There is nothing more deceptive than an obvious fact.',
+    'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
+    'I never make exceptions. An exception disproves the rule.',
+    'What one man can invent another can discover.',
+    'Nothing clears up a case so much as stating it to another person.',
+    'Education never ends, Watson. It is a series of lessons, with the greatest for the last.'
+];
+
+//empty array to store the words inputed
+let words = [];
+let wordIndex = 0;
+
+let sartTime = Date.now();
+
+const quoteElement = document.getElementById('quote');
+const messageElement = document.getElementById('message');
+const typedValueElement = document.getElementById('typed-value');
